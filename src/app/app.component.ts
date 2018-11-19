75a8719a55ffdee7874e03644e24532ad499099e
SudarshanChowdhary <sudarshan@gmail.com>
2018-11-20 02:04:45 +0530

    Added chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
     });
   
   }
+  
   private pieGraphOptions: Object = {
     gradient: false,
     animations: true,
@@ -25,6 +26,39 @@ export class AppComponent implements OnInit {
     legendPosition: "right",
     doughnut: false
   };
+
+  single: any[];
+  multi: any[];
+  view: any[] = [500, 400];
+
+  private multiLineOptions: Object = {
+  showXAxis : true,
+  showYAxis : true,
+  gradient : true,
+  showLegend : true,
+  showXAxisLabel : true,
+  showYAxisLabel : true,
+  colorScheme : {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  },
+
+  // line, area
+  autoScale : true,
+  
+  // constructor() {
+  //   Object.assign(this, single, multi)   
+  // }
+  }
+  private contractProcessedData: Array<any> =[
+  {
+    name: "contractProcessed",
+    series:[{name:"August", value:8}, {name:"September", value:9}, {name:"October", value:17}, {name:"November", value:0}]
+  },
+  {
+    name: "avgTeamWork",
+    series:[{name:"August", value:8}, {name:"September", value:8}, {name:"October", value:8}, {name:"November", value:8}]
+  }  
+]
   private groupedVerticalBarGraphOptions: Object = {
     scheme: {
       domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
@@ -96,6 +130,278 @@ export class AppComponent implements OnInit {
 
 
   ];
+
+
+private turnaroundTimeOptions: Object = {
+ 
+  // options
+  showXAxis : true,
+  showYAxis : true,
+  gradient : true,
+  showLegend : true,
+  showXAxisLabel : true,
+ 
+  showYAxisLabel : true,
+ 
+  colorScheme : {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  }
+
+};
+
+  private turnaroundTimeData: Array<any> =[
+    {
+      "value": 0,
+      "name": "Ankur Yadav"
+    },
+    {
+      "value": 1,
+      "name": "Kajal Gadiya"
+    },
+    {
+      "value": 1,
+      "name": "Kapil Gupta"
+    },
+    {
+      "value": 0,
+      "name": "Manisha Saxena"
+    },
+    {
+      "value": 0,
+      "name": "Mohit Magarde"
+    },
+    {
+      "value": 0,
+      "name": "Pavithra"
+    },
+    {
+      "value": 0,
+      "name": "Rajesh"
+    },
+    {
+      "value": 0,
+      "name": "Shambhavi Mishra"
+    },
+    {
+      "value": 0,
+      "name": "Shrutika"
+    },
+    {
+      "value": 0,
+      "name": "Vishal Chavan"
+    }]
+
+    private analystErrorsOptions: Object = {
+      showXAxis : true,
+      showYAxis : true,
+      gradient : true,
+      showLegend : true,
+      showXAxisLabel : true,
+      showYAxisLabel : true,
+      colorScheme : {
+        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+      },
+    
+      // line, area
+      autoScale : true,  
+    };
+    
+    private analystErrorsData: Array<any> = [
+      { name:"empError",
+        series:[{
+            "value": 0,
+            "name": "Ankur Yadav"
+          },
+          {
+            "value": 0,
+            "name": "Kajal Gadiya"
+          },
+          {
+            "value": 0,
+            "name": "Kapil Gupta"
+          },
+          {
+            "value": 0,
+            "name": "Manisha Saxena"
+          },
+          {
+            "value": 0,
+            "name": "Mohit Magarde"
+          },
+          {
+            "value": 0,
+            "name": "Pavithra"
+          },
+          {
+            "value": 0,
+            "name": "Rajesh"
+          },
+          {
+            "value": 0,
+            "name": "Shambhavi Mishra"
+          },
+          {
+            "value": 0,
+            "name": "Shrutika"
+          },
+          {
+            "value": 0,
+            "name": "Vishal Chavan"
+          }
+        ]
+      },
+      {
+        name:"Team Error", series:[
+          {
+            "value": 0,
+            "name": "Ankur Yadav"
+          },
+          {
+            "value": 0,
+            "name": "Kajal Gadiya"
+          },
+          {
+            "value": 0,
+            "name": "Kapil Gupta"
+          },
+          {
+            "value": 0,
+            "name": "Manisha Saxena"
+          },
+          {
+            "value": 0,
+            "name": "Mohit Magarde"
+          },
+          {
+            "value": 0,
+            "name": "Pavithra"
+          },
+          {
+            "value": 0,
+            "name": "Rajesh"
+          },
+          {
+            "value": 0,
+            "name": "Shambhavi Mishra"
+          },
+          {
+            "value": 0,
+            "name": "Shrutika"
+          },
+          {
+            "value": 0,
+            "name": "Vishal Chavan"
+          }
+        ]
+      }
+    ]
+
+    private slaPerformanceOptions: Object = {
+      showXAxis : true,
+      showYAxis : true,
+      gradient : true,
+      showLegend : true,
+      showXAxisLabel : true,
+      showYAxisLabel : true,
+      colorScheme : {
+        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+      },
+    
+      // line, area
+      autoScale : true,  
+    };
+
+    private slaPerformanceData: Array<any> = [
+      { name:"SLA",
+        series:[{
+            "value": 25,
+            "name": "Ankur Yadav"
+          },
+          {
+            "value": 100,
+            "name": "Kajal Gadiya"
+          },
+          {
+            "value": 100,
+            "name": "Kapil Gupta"
+          },
+          {
+            "value": 0,
+            "name": "Manisha Saxena"
+          },
+          {
+            "value": 0,
+            "name": "Mohit Magarde"
+          },
+          {
+            "value": 16,
+            "name": "Pavithra"
+          },
+          {
+            "value": 36,
+            "name": "Rajesh"
+          },
+          {
+            "value": 0,
+            "name": "Shambhavi Mishra"
+          },
+          {
+            "value": 37,
+            "name": "Shrutika"
+          },
+          {
+            "value": 0,
+            "name": "Vishal Chavan"
+          }
+        ]
+      },
+      {
+        name:"AvgTeamSLA", series:[
+          {
+            "value": 31,
+            "name": "Ankur Yadav"
+          },
+          {
+            "value": 31,
+            "name": "Kajal Gadiya"
+          },
+          {
+            "value": 31,
+            "name": "Kapil Gupta"
+          },
+          {
+            "value": 31,
+            "name": "Manisha Saxena"
+          },
+          {
+            "value": 31,
+            "name": "Mohit Magarde"
+          },
+          {
+            "value": 31,
+            "name": "Pavithra"
+          },
+          {
+            "value": 31,
+            "name": "Rajesh"
+          },
+          {
+            "value": 31,
+            "name": "Shambhavi Mishra"
+          },
+          {
+            "value": 31,
+            "name": "Shrutika"
+          },
+          {
+            "value": 31,
+            "name": "Vishal Chavan"
+          }
+        ]
+      }
+    ]
+
+
   public bookOfWorkData: Array<object> = [{'name': 'completed', 'value': 34}, {'name': 'workInProgress', 'value': 103}, {'name': 'unassigned', 'value': 0}, {'name': 'exception', 'value': 0}];
 
   private multiLineGraphOptions;
