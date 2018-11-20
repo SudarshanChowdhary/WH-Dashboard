1e3ca5dbde0f945d0097ca491e15988b780bf4bd
Koyalkar <Goverdhan.Koyalkar@searshc.com>
2018-11-21 02:39:37 +0530

    data formatted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,8 @@ import { HolmesService } from './Holmes.service';
   styleUrls: ['./app.component.css', '../../node_modules/nvd3/build/nv.d3.css'],
   providers: [HolmesService]
 })
+
 export class AppComponent implements OnInit {
-  constructor(private HolmesService: HolmesService) {
-    this.HolmesService.getData().subscribe(data => {
-      this.drawPieGraph(data.bookOfWork);
-      this.drawMultiBarGraph(data.completionStatus);
-      this.drawMultiLineGraph(data.contractProcressed)
-    });
-  
-  }
-  
   private pieGraphOptions: Object = {
     gradient: false,
     animations: true,
@@ -26,39 +18,52 @@ export class AppComponent implements OnInit {
     legendPosition: "right",
     doughnut: false
   };
+  private bookOfWorkData: Array<object>;
+  private completionStatusData: Array<any>;
 
-  single: any[];
-  multi: any[];
-  view: any[] = [500, 400];
+  constructor(private HolmesService: HolmesService) {
+    this.HolmesService.getData().subscribe(data => {
+      this.bookOfWorkData = this.formatBookOfWorkData(data.bookOfWork);
+      this.completionStatusData = this.formatCompletionStatusData(data.completionStatus);
+      this.contractProcessedData = this.formatContractProcessedData(data.contractProcessed);
+      this.turnaroundTimeData = this.formatTurnaroundTimeData(data.turnaroundTime);
+      this.analystErrorsData = this.formatAnalystErrorsData(data.analystErrors);
+      this.slaPerformanceData = this.formatSLAPerformanceData(data.slaPerformance);
+    });
+  }
 
   private multiLineOptions: Object = {
-  showXAxis : true,
-  showYAxis : true,
-  gradient : true,
-  showLegend : true,
-  showXAxisLabel : true,
-  showYAxisLabel : true,
-  colorScheme : {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  },
-
-  // line, area
-  autoScale : true,
-  
-  // constructor() {
-  //   Object.assign(this, single, multi)   
-  // }
+    showXAxis: true,
+    showYAxis: true,
+    gradient: true,
+    showLegend: true,
+    showXAxisLabel: true,
+    showYAxisLabel: true,
+    colorScheme: {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    },
+    autoScale: true,
   }
-  private contractProcessedData: Array<any> =[
-  {
-    name: "contractProcessed",
-    series:[{name:"August", value:8}, {name:"September", value:9}, {name:"October", value:17}, {name:"November", value:0}]
-  },
-  {
-    name: "avgTeamWork",
-    series:[{name:"August", value:8}, {name:"September", value:8}, {name:"October", value:8}, {name:"November", value:8}]
-  }  
-]
+  private contractProcessedData: Array<any>;
+  formatContractProcessedData(data) {
+    let temp = [];
+    for (let key in data) {
+      let series = [];
+      if (key == "avgTeamWork") {
+        for (let i = 0; i < data["lstContractProcessed"].length; i++) {
+          series.push({ name: data["lstContractProcessed"][i].timeScale, value: data["avgTeamWork"] });
+        }
+        temp.push({ "name": key, "series": series });
+      } else {
+        for (let i = 0; i < data[key].length; i++) {
+          series.push({ name: data[key][i].timeScale, value: data[key][i].contractProcessed })
+        }
+        temp.push({ "name": key, "series": series });
+      }
+    }
+    console.log(temp);
+    return temp;
+  }
   private groupedVerticalBarGraphOptions: Object = {
     scheme: {
       domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
@@ -74,353 +79,132 @@ export class AppComponent implements OnInit {
     xAxisLabel: "",
     yAxisLabel: ""
   };
-  private completionStatusData: Array<any> =[
-    {
-      "name": "August",
-      "series": [
-        {
-          "name": "Contract Allocated",
-          "value": 8
-        },
-        {
-          "name": "Contract Completed",
-          "value": 8
+
+  formatCompletionStatusData(data) {
+    let temp = [];
+    for (let i = 0; i < data.length; i++) {
+      let series = [];
+      for (let key in data[i]) {
+        if (key != "timeScale") {
+          series.push({ "name": key, "value": data[i][key] });
         }
-      ]
-    },
-    {
-      "name": "September",
-      "series": [
-        {
-          "name": "Contract Allocated",
-          "value": 9
-        },
-        {
-          "name": "Contract Completed",
-          "value": 9
-        }
-      ]
-    },
-    {
-      "name": "October",
-      "series": [
-        {
-          "name": "Contract Allocated",
-          "value": 97
-        },
-        {
-          "name": "Contract Completed",
-          "value": 17
-        }
-      ]
-    },
-    {
-      "name": "November",
-      "series": [
-        {
-          "name": "Contract Allocated",
-          "value": 32
-        },
-        {
-          "name": "Contract Completed",
-          "value": 0
-        }
-      ]
+      }
+      temp.push({ name: data[i].timeScale, series: series });
     }
-
-
-  ];
-
-
-private turnaroundTimeOptions: Object = {
- 
-  // options
-  showXAxis : true,
-  showYAxis : true,
-  gradient : true,
-  showLegend : true,
-  showXAxisLabel : true,
- 
-  showYAxisLabel : true,
- 
-  colorScheme : {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    return temp;
   }
 
-};
+  private turnaroundTimeOptions: Object = {
 
-  private turnaroundTimeData: Array<any> =[
-    {
-      "value": 0,
-      "name": "Ankur Yadav"
-    },
-    {
-      "value": 1,
-      "name": "Kajal Gadiya"
-    },
-    {
-      "value": 1,
-      "name": "Kapil Gupta"
-    },
-    {
-      "value": 0,
-      "name": "Manisha Saxena"
-    },
-    {
-      "value": 0,
-      "name": "Mohit Magarde"
-    },
-    {
-      "value": 0,
-      "name": "Pavithra"
-    },
-    {
-      "value": 0,
-      "name": "Rajesh"
-    },
-    {
-      "value": 0,
-      "name": "Shambhavi Mishra"
-    },
-    {
-      "value": 0,
-      "name": "Shrutika"
-    },
-    {
-      "value": 0,
-      "name": "Vishal Chavan"
-    }]
+    // options
+    showXAxis: true,
+    showYAxis: true,
+    gradient: true,
+    showLegend: true,
+    showXAxisLabel: true,
 
-    private analystErrorsOptions: Object = {
-      showXAxis : true,
-      showYAxis : true,
-      gradient : true,
-      showLegend : true,
-      showXAxisLabel : true,
-      showYAxisLabel : true,
-      colorScheme : {
-        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-      },
-    
-      // line, area
-      autoScale : true,  
-    };
-    
-    private analystErrorsData: Array<any> = [
-      { name:"empError",
-        series:[{
-            "value": 0,
-            "name": "Ankur Yadav"
-          },
-          {
-            "value": 0,
-            "name": "Kajal Gadiya"
-          },
-          {
-            "value": 0,
-            "name": "Kapil Gupta"
-          },
-          {
-            "value": 0,
-            "name": "Manisha Saxena"
-          },
-          {
-            "value": 0,
-            "name": "Mohit Magarde"
-          },
-          {
-            "value": 0,
-            "name": "Pavithra"
-          },
-          {
-            "value": 0,
-            "name": "Rajesh"
-          },
-          {
-            "value": 0,
-            "name": "Shambhavi Mishra"
-          },
-          {
-            "value": 0,
-            "name": "Shrutika"
-          },
-          {
-            "value": 0,
-            "name": "Vishal Chavan"
+    showYAxisLabel: true,
+
+    colorScheme: {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    }
+
+  };
+
+  private turnaroundTimeData: Array<any>;
+  formatTurnaroundTimeData(data) {
+    let temp = [];
+    for (let i = 0; i < data.lstTurnaroundTime.length; i++) {
+      temp.push({ "name": data.lstTurnaroundTime[i].userInfo, "value": data.lstTurnaroundTime[i].avgTime });
+    }
+    return temp;
+  }
+
+  private analystErrorsOptions: Object = {
+    showXAxis: true,
+    showYAxis: true,
+    gradient: true,
+    showLegend: true,
+    showXAxisLabel: true,
+    showYAxisLabel: true,
+    colorScheme: {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    },
+
+    // line, area
+    autoScale: true,
+  };
+
+  formatAnalystErrorsData(data) {
+    let temp = [];
+    for (let key in data) {
+      let series = [];
+      if (key == "avgTeamError") {
+        for (let i = 0; i < data["lstAnalystErrors"].length; i++) {
+          series.push({ name: data["lstAnalystErrors"][i].userInfo, value: data["avgTeamError"] });
+        }
+        temp.push({ "name": key, "series": series });
+      } else {
+        if (key != "timeScale") {
+          for (let i = 0; i < data[key].length; i++) {
+            series.push({ name: data[key][i].userInfo, value: data[key][i].empError })
           }
-        ]
-      },
-      {
-        name:"Team Error", series:[
-          {
-            "value": 0,
-            "name": "Ankur Yadav"
-          },
-          {
-            "value": 0,
-            "name": "Kajal Gadiya"
-          },
-          {
-            "value": 0,
-            "name": "Kapil Gupta"
-          },
-          {
-            "value": 0,
-            "name": "Manisha Saxena"
-          },
-          {
-            "value": 0,
-            "name": "Mohit Magarde"
-          },
-          {
-            "value": 0,
-            "name": "Pavithra"
-          },
-          {
-            "value": 0,
-            "name": "Rajesh"
-          },
-          {
-            "value": 0,
-            "name": "Shambhavi Mishra"
-          },
-          {
-            "value": 0,
-            "name": "Shrutika"
-          },
-          {
-            "value": 0,
-            "name": "Vishal Chavan"
-          }
-        ]
+          temp.push({ "name": key, "series": series });
+        }
       }
-    ]
+    }
+    console.log(temp);
+    return temp;
+  }
 
-    private slaPerformanceOptions: Object = {
-      showXAxis : true,
-      showYAxis : true,
-      gradient : true,
-      showLegend : true,
-      showXAxisLabel : true,
-      showYAxisLabel : true,
-      colorScheme : {
-        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-      },
-    
-      // line, area
-      autoScale : true,  
-    };
-
-    private slaPerformanceData: Array<any> = [
-      { name:"SLA",
-        series:[{
-            "value": 25,
-            "name": "Ankur Yadav"
-          },
-          {
-            "value": 100,
-            "name": "Kajal Gadiya"
-          },
-          {
-            "value": 100,
-            "name": "Kapil Gupta"
-          },
-          {
-            "value": 0,
-            "name": "Manisha Saxena"
-          },
-          {
-            "value": 0,
-            "name": "Mohit Magarde"
-          },
-          {
-            "value": 16,
-            "name": "Pavithra"
-          },
-          {
-            "value": 36,
-            "name": "Rajesh"
-          },
-          {
-            "value": 0,
-            "name": "Shambhavi Mishra"
-          },
-          {
-            "value": 37,
-            "name": "Shrutika"
-          },
-          {
-            "value": 0,
-            "name": "Vishal Chavan"
+  formatSLAPerformanceData(data) {
+    let temp = [];
+    for (let key in data) {
+      let series = [];
+      if (key == "avgTeamSLA") {
+        for (let i = 0; i < data["lstSLAPerformance"].length; i++) {
+          series.push({ name: data["lstSLAPerformance"][i].userInfo, value: data["avgTeamSLA"] });
+        }
+        temp.push({ "name": key, "series": series });
+      } else {
+        if (key != "timeScale") {
+          for (let i = 0; i < data[key].length; i++) {
+            series.push({ name: data[key][i].userInfo, value: data[key][i].empSLA })
           }
-        ]
-      },
-      {
-        name:"AvgTeamSLA", series:[
-          {
-            "value": 31,
-            "name": "Ankur Yadav"
-          },
-          {
-            "value": 31,
-            "name": "Kajal Gadiya"
-          },
-          {
-            "value": 31,
-            "name": "Kapil Gupta"
-          },
-          {
-            "value": 31,
-            "name": "Manisha Saxena"
-          },
-          {
-            "value": 31,
-            "name": "Mohit Magarde"
-          },
-          {
-            "value": 31,
-            "name": "Pavithra"
-          },
-          {
-            "value": 31,
-            "name": "Rajesh"
-          },
-          {
-            "value": 31,
-            "name": "Shambhavi Mishra"
-          },
-          {
-            "value": 31,
-            "name": "Shrutika"
-          },
-          {
-            "value": 31,
-            "name": "Vishal Chavan"
-          }
-        ]
+          temp.push({ "name": key, "series": series });
+        }
       }
-    ]
+    }
+    console.log(temp);
+    return temp;
+  }
 
+  private analystErrorsData: Array<any>;
+  private slaPerformanceOptions: Object = {
+    showXAxis: true,
+    showYAxis: true,
+    gradient: true,
+    showLegend: true,
+    showXAxisLabel: true,
+    showYAxisLabel: true,
+    colorScheme: {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    },
 
-  public bookOfWorkData: Array<object> = [{'name': 'completed', 'value': 34}, {'name': 'workInProgress', 'value': 103}, {'name': 'unassigned', 'value': 0}, {'name': 'exception', 'value': 0}];
+    // line, area
+    autoScale: true,
+  };
 
+  private slaPerformanceData: Array<any>;
   private multiLineGraphOptions;
-
   async ngOnInit() {
   }
 
-  drawMultiBarGraph(data) {
-
-  }
-
-  drawMultiLineGraph(data) {
-
-  }
-
-
-  drawPieGraph(data) {
+  formatBookOfWorkData(data) {
+    let temp = [];
     for (let key in data) {
-      this.bookOfWorkData.push({"name": key, "value": data[key]});
+      temp.push({ "name": key, "value": data[key] });
     }
+    return temp;
   }
 }
